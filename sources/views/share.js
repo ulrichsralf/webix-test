f97ba5a5523a2f7ccaae18cb4889bf49925b9164
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2020-03-08 00:09:30 +0100

    debug

--- sources/views/share.js
+++ sources/views/share.js
@@ -1,5 +1,4 @@
 import {JetView} from "webix-jet";
-import CustomBackend from "./CustomBackend";
 
 export default class MyView extends JetView {
     config() {
@@ -9,37 +8,31 @@ export default class MyView extends JetView {
             "//cdn.webix.com/pro/edge/filemanager/filemanager.css": true
         }).then(() => {
 
-            class CustomBackend extends fileManager.services.Backend {
-                // you can use calls to real backend server
-                files(id) {
-                    return webix.ajax("//docs.webix.com/filemanager-backend/files", { id });
-                }
-                // or just return a data promise
-                getInfo() {
-                    return Promise.resolve({
-                        stats:{
-                            free: 52 * 1000 * 1000,
-                            total: 250 * 1000 * 1000,
-                            used: 198.4 * 1000 * 1000,
-                        }
-                    });
-                }
-                // some methods may require remote url, instead of data
-                icon(type, size) {
-                    //serve image/jpg for all icons
-                    return `//docs.webix.com/filemanager-backend/icons/${size || "small"}/image/jpg.svg`;
-                }
-            }
+
             return {
                 view: "filemanager",
+                id: "fm",
                 // url: "https://docs.webix.com/filemanager-backend/"
                 url: "http://localhost:8081/",
-                override: new Map([[fileManager.services.Backend, CustomBackend]]),
+                // override: new Map([[fileManager.services.Backend, CustomBackend]]),
             };
+
         });
     }
 
     init(view) {
-
+        // const file = {
+        //     value: "pixeljuice.pdf",
+        //     date: new Date(),
+        //     size: 7231,
+        //     id: "/pixeljuice.pdf",
+        //     type: "document"
+        // };
+        // var fm = this.$$("fm");
+        // fm.attachEvent("onChange", function () {
+        //     console.log("state " + JSON.stringify(fm.getState()));
+        // });
+        // fm.getService("operations").open(file);
+        // this.$$("$template7").hide();
     }
 }
